cb5dad988a2be5f6385deba720ef5a62d3a6ea51
uyennguyen87 <uyen.nguyen.87@hotmail.com>
2016-05-20 23:54:07 +0700

    Chapter1 - handle card toggle via state

--- app/Card.js
+++ app/Card.js
@@ -2,16 +2,32 @@ import React, { Component } from 'react';
 import CheckList from './CheckList';
 
 export default class Card extends Component {
+    constructor() {
+        super(...arguments);
+        this.state = {
+            showDetails: false
+        }
+    }
     render() {
-        return (
-            <div className="card">
-                <div className="card__title">{this.props.title}</div>
+        let cardDetails;
+        if(this.state.showDetails) {
+            cardDetails = (
                 <div className="card__details">
                     {this.props.description}
                     <CheckList
                         cardId={this.props.id}
                         tasks={this.props.tasks} />
                 </div>
+            );
+        }
+        return (
+            <div className="card">
+                <div onClick={
+                    () => this.setState({showDetails: !this.state.showDetails})
+                    } className="card__title">
+                    {this.props.title}
+                </div>
+                {cardDetails}
             </div>
         );
     }
