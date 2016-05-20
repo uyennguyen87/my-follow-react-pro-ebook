0f36d4687be533787b7f6e8cd35d09ea75e3d78b
uyen_nguyen <uyen.nguyen@axonactive.vn>
2016-05-20 17:46:46 +0700

    chapter One

---
+++ app/CheckList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class CheckList extends Component {
+    render() {
+        let cardId = this.props.cardId;
+        let tasks = this.props.tasks.map(task => {
+            const key = `${cardId}_${task.id}`;
+
+            return  <li key={key} className="checklist__task">
+                <input type="checkbox" defaultChecked={task.done} />
+                {task.name}
+                <a href="#" className="checkList__task--remove" />
+            </li>
+        });
+
+        return (
+            <div className="checklist">
+                <ul>{tasks}</ul>
+            </div>
+        );
+    }
+}
